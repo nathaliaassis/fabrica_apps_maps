9543d1baf4436e43880435a871debf8aa9f1032d
unknown <nathalia.assis@doutorhoje.com.br>
2020-04-25 22:49:32 -0300

    novo marker com class component

--- App.js
+++ App.js
@@ -1,80 +1,103 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 
-export default function App() {
-  const [region, setRegion] = useState({
-    latitude: -15.7768263,
-    longitude: -47.9008514,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
-  });
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-  function alterarCidade(lat, long) {
-    setRegion({
-      latitude: lat,
-      longitude: long,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    });
+    this.state = {
+      region: {
+        latitude: -15.7768263,
+        longitude: -47.9008514,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+
+      markers: [
+        {
+          key: 0,
+          image: require('./src/assets/img/carro.png'),
+          coords: { latitude: -15.7768263, longitude: -47.9008514 },
+        },
+        {
+          key: 1,
+          image: require('./src/assets/img/carro_left.png'),
+          coords: { latitude: -15.7868263, longitude: -47.9208514 },
+        },
+        {
+          key: 2,
+          image: require('./src/assets/img/carro_down.png'),
+          coords: { latitude: -15.7968263, longitude: -47.9508514 },
+        },
+      ],
+    };
+    this.alterarCidade = this.alterarCidade.bind(this);
+    this.newMarker = this.newMarker.bind(this);
   }
 
-  const [markers, setMarkers] = useState([
-    {
-      key: 0,
-      coords: { latitude: -15.7768263, longitude: -47.9008514 },
-      pinColor: '#ff0000',
-    },
-    {
-      key: 1,
-      coords: { latitude: -15.7868263, longitude: -47.9208514 },
-      pinColor: '#0000ff',
-    },
-    {
-      key: 2,
-      coords: { latitude: -15.7968263, longitude: -47.9508514 },
-      pinColor: '#00ff00',
-    },
-  ]);
+  newMarker(e) {
+    let state = this.state;
 
-  function newMarker(e) {
-    markers.push({
-      key: markers.length,
+    state.markers.push({
+      key: state.markers.length,
       coords: {
         latitude: e.nativeEvent.coordinate.latitude,
         longitude: e.nativeEvent.coordinate.longitude,
       },
       pinColor: '#ff0000',
     });
-    setMarkers(markers);
+
+    this.setState(state);
   }
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}> MyMap</Text>
-      <MapView
-        style={styles.maps}
-        region={region}
-        onPress={(e) => newMarker(e)}>
-        {markers.map((m) => {
-          return (
-            <Marker key={m.key} coordinate={m.coords} pinColor={m.pinColor} />
-          );
-        })}
-      </MapView>
-      <View style={styles.areaBtns}>
-        <TouchableOpacity
-          style={styles.btn}
-          onPress={() => alterarCidade(-23.5492243, -46.5813785)}>
-          <Text style={styles.btnText}>São Paulo</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.btn}
-          onPress={() => alterarCidade(-15.7768263, -47.9008514)}>
-          <Text style={styles.btnText}>Brasília</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+
+  alterarCidade(lat, long) {
+    let state = this.state;
+
+    let region = {
+      latitude: lat,
+      longitude: long,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    };
+    state.region = region;
+    this.setState(state);
+  }
+  render() {
+    const { region, markers } = this.state;
+    return (
+      <ScrollView style={styles.container}>
+        <Text style={styles.title}> MyMap</Text>
+        <MapView style={styles.maps} region={region} onPress={this.newMarker}>
+          {markers.map((m) => {
+            return <Marker image={m.image} key={m.key} coordinate={m.coords} />;
+          })}
+        </MapView>
+        <View style={styles.areaBtns}>
+          <TouchableOpacity
+            style={styles.btn}
+            onPress={() => {
+              this.alterarCidade(-23.5492243, -46.5813785);
+            }}>
+            <Text style={styles.btnText}>São Paulo</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.btn}
+            onPress={() => {
+              this.alterarCidade(-15.7768263, -47.9008514);
+            }}>
+            <Text style={styles.btnText}>Brasília</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
